d7dcd9b8a3a8cc89816c2a321a673bd9a6ff3d28
engle212 <70217270+engle212@users.noreply.github.com>
2024-12-23 21:46:55 +0000

    Deploying to gh-pages from @ engle212/engle212.github.io@4dcbddc478ee3640f806d904eaa111b958b64db5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -382,7 +382,12 @@ ninja.data = [{
           title: 'project 1',
           description: "with background image",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
+              window.location.href = "/projects/10_project/";
+            },},{id: "projects-structured-light-camera",
+          title: 'Structured Light Camera',
+          description: "My capstone project at OSU",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/1_slc/";
             },},{id: "projects-project-2",
           title: 'project 2',
           description: "a project with a background image and giscus comments",
@@ -393,21 +398,6 @@ ninja.data = [{
           description: "a project that redirects to another website",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
             },},{id: "projects-project-7",
           title: 'project 7',
           description: "with background image",
@@ -418,11 +408,6 @@ ninja.data = [{
           description: "an other project with a background image and giscus comments",
           section: "Projects",handler: () => {
               window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
             },},{
         id: 'social-email',
         title: 'email',
