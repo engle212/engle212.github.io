d1fe1ec9fc6606e1ca30649aa0bb095ff8a97322
engle212 <70217270+engle212@users.noreply.github.com>
2024-12-24 19:02:35 +0000

    Deploying to gh-pages from @ engle212/engle212.github.io@d12940f92e90a819185c6c92fbb09ce4aa1a10b1 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,13 +23,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
-        },{id: "nav-repositories",
-          title: "repositories",
-          description: "Some of my publicly available GitHub repositories",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/repositories/";
-          },
         },{id: "nav-cv",
           title: "cv",
           description: "",
